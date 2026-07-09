87b8df765140070f0ac7940d552af114a01749f8
unknown <kelvinkokorie@gmail.com>
2026-07-09 13:26:07 +0100

    new ui and functionality fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,36 @@ export default function App(){
   })
   const [tickets, setTicket] = useState<Tickets[]>([])
 
+  const [editingIndex, setEditingIndex] = useState<number | null>(null)
+
   const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
     const {value, name} = event.target
     setForm({...form, [name]: value})
   }
+  ///has for update if elsemto check if the button clicked is for update or add new item
   const handleSubmit = (event:FormEvent<HTMLFormElement> ) => {
      event.preventDefault()
-    const newTicket :Tickets = {
-      title: form.title,
-      description: form.description
-    }
-    setTicket([
-      ...tickets,
-      newTicket
+     if (editingIndex === null){
 
-    ])
+       const newTicket :Tickets = {
+         title: form.title,
+         description: form.description
+       }
+       setTicket([
+         ...tickets,
+         newTicket
+   
+       ])
+     }else{
+      const updatedTicket = [...tickets]
+      updatedTicket [editingIndex]= {
+        title:form.title,
+        description: form.description
+      }
+      setTicket(updatedTicket)
+      setEditingIndex(null)
+
+     }
     setForm({
       title:"",
       description:""
@@ -38,12 +53,20 @@ const handleDelete = (id: number) => {
 
   })
 }
+const handleEdit = (index: number) =>{
+  setEditingIndex(index)
+  setForm(tickets[index])
+  
+}
+
   return(
-    <div className="border grid justify-center item-center gap-4 min-h-screen ">
-      <Form handleChange={handleChange} handleSubmit={handleSubmit} form={form} />
+    <div className=" flex flex-col justify-center items-center gap-4 min-h-screen p-4 bg-gray-100">
+      <div className="w-full max-w-2xl">
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} form={form} editingIndex={editingIndex} />
       {tickets.map ((item, index)=>(
-        <TicketCard key={index} ticket={item} onDelete={handleDelete} index={index} />
+        <TicketCard key={index} ticket={item} onDelete={handleDelete} index={index} onEdit={handleEdit}/>
       ))}
+      </div>
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,7 @@
 import {  type ChangeEvent } from "react"
 import { type FormEvent } from "react"
 import "../index.css"
-import type Ticket from "../type"
+
   type FormProps = {
     form: {
       title: string
@@ -9,17 +9,18 @@ import type Ticket from "../type"
     }
     handleChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
     handleSubmit: (event: FormEvent<HTMLFormElement> ) => void
+    editingIndex: number | null
     
   }
 
-export default function Form({form, handleChange, handleSubmit}: FormProps  ){
+export default function Form({form, handleChange, handleSubmit, editingIndex}: FormProps  ){
     return  (
     <div>
-      <h1 className="">My Ticket App</h1>
-      <form action="submit" onSubmit={handleSubmit} className="flex">
-      <input name="title" placeholder="name"  value={form.title} onChange={handleChange}/>
-      <textarea name="description" value={form.description} onChange={handleChange} />
-      <button> click</button> 
+      <h1 className="text-3xl font-bold mb-6 text-center">KelvinDesk </h1>
+      <form action="submit" onSubmit={handleSubmit} className=" bg-white p-6 rounded-xl shadow-md flex flex-col gap-4">
+      <input name="title" placeholder="name"  value={form.title} onChange={handleChange} className="border rounded-lg p-3 w-full"/>
+      <textarea name="description" value={form.description} onChange={handleChange} className="border rounded-lg p-3 w-full"/>
+      <button className="bg-black w-full text-white p-3 rounded-lg" disabled={form.title.trim() === "" || form.description.trim() === ""}>{ editingIndex === null ? "Create Ticket": "Update Ticket"}  </button> 
       </form>
       </div>
       )
